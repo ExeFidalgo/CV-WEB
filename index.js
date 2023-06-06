b8e5d469aa3a2e8455c5a9a56308fc19d9dc71ac
Ezequiel Fidalgo <exekover@gmail.com>
2023-06-06 14:31:49 -0300

    POSITIVE VALUE

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const form = document.getElementById('myForm');
 const dataTable = document.getElementById('dataTable');
 
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
-  const salary = document.getElementById('salary').value;
+  const salaryInput = document.getElementById('salary');
+  const salary = parseFloat(salaryInput.value);
 
-  if (name.trim() === '' || email.trim() === '' || message.trim() === '' || salary.trim() === '') {
+  if (name.trim() === '' || email.trim() === '' || message.trim() === '' || salaryInput.value.trim() === '') {
     alert('Por favor, complete todos los campos');
+    return;
+  }
+
+  if (salary < 0) {
+    alert('El salario debe ser un valor numérico positivo');
     return;
   }
 
@@ -17,10 +24,10 @@ form.addEventListener('submit', (event) => {
   row.insertCell(0).textContent = name;
   row.insertCell(1).textContent = email;
   row.insertCell(2).textContent = message;
-  row.insertCell(3).textContent = salary;
+  row.insertCell(3).textContent = (salary >= 0) ? salary.toFixed(2) : 'OFERTA RECHAZADA';
 
-  const usdSalary = (parseFloat(salary) / 500).toFixed(2);
-  const eurSalary = (parseFloat(salary) / 510).toFixed(2);
+  const usdSalary = (salary / 500).toFixed(2);
+  const eurSalary = (salary / 510).toFixed(2);
   row.insertCell(4).textContent = `$ ${usdSalary} USD`;
   row.insertCell(5).textContent = `€ ${eurSalary} EUR`;
 
